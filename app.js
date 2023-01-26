75b98d4560fe35b5f9707f56d1d402bb49921ff9
Marshal <olmarsh.192@gmail.com>
2023-01-26 16:05:54 +0100

    added swaggerUI

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
+
 const express = require("express");
 const app = express();
 
@@ -35,10 +40,14 @@ app.use(
   })
 );
 
-// routes
+// Home Route
 app.get("/", (req, res) => {
-  res.send("Marshal's Job API");
+  res.send("<h1>Marshal's Job API</h1><a href='/api-docs'>Documentation</a>");
 });
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
